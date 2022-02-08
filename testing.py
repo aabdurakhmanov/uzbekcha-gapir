28a9af435921789ad989580f69da962e18b31754
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-02-08 22:24:17 +0500

    create testing file

---
+++ testing.py
@@ -0,0 +1,68 @@
+from tkinter import *
+
+ws = Tk()
+ws.title('PythonGuides')
+ws.geometry('400x300')
+ws.config(bg='#F25252')
+
+frame = Frame(ws)
+
+text_box = Text(
+    ws,
+    height=13,
+    width=32,
+    font=(12)
+)
+
+text_box.pack(side=LEFT,expand=True)
+
+
+sb_ver = Scrollbar(
+    ws,
+    orient=VERTICAL
+    )
+
+sb_ver.pack(side=RIGHT, fill=Y)
+
+text_box.config(yscrollcommand=sb_ver.set)
+sb_ver.config(command=text_box.yview)
+
+
+ws.mainloop()
+# import tkinter
+# import tkinter.ttk as ttk
+#
+# class TextScrollCombo(ttk.Frame):
+#
+#     def __init__(self, *args, **kwargs):
+#
+#         super().__init__(*args, **kwargs)
+#
+#     # ensure a consistent GUI size
+#         self.grid_propagate(False)
+#     # implement stretchability
+#         self.grid_rowconfigure(0, weight=1)
+#         self.grid_columnconfigure(0, weight=1)
+#
+#     # create a Text widget
+#         self.txt = tkinter.Text(self)
+#         self.txt.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
+#
+#     # create a Scrollbar and associate it with txt
+#         scrollb = ttk.Scrollbar(self, command=self.txt.yview)
+#         scrollb.grid(row=1, column=2, sticky='nsew')
+#         self.txt['yscrollcommand'] = scrollb.set
+#
+# main_window = tkinter.Tk()
+#
+# combo = TextScrollCombo(main_window)
+# combo.pack(fill="both", expand=False)
+# combo.config(width=600, height=600)
+#
+# combo.txt.config(font=("consolas", 12), undo=True, wrap='word')
+# combo.txt.config(borderwidth=3, relief="sunken")
+#
+# style = ttk.Style()
+# style.theme_use('clam')
+#
+# main_window.mainloop()
