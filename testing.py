ccec78015aa5d12f1004b0b9915ffc554a2c7516
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2022-02-26 22:16:58 +0500

    testing some func

--- testing.py
+++ testing.py
@@ -1,3 +1,28 @@
+import tkinter as tk
+
+
+class MainWindow:
+    def __init__(self, master):
+        self.master = master
+
+        self.frame = tk.Frame(self.master)
+        self.frame.pack()
+
+        self.label = tk.Label(self.frame, text = 'Scrol bla bla')
+        self.label.pack()
+
+        self.scrollbar = tk.Scrollbar(self.frame)
+        self.scrollbar.pack(side = tk.RIGHT, fill = tk.Y)
+
+        self.text = tk.Text(self.frame, yscrollcommand = self.scrollbar.set)
+        self.text.pack()
+
+        self.scrollbar.config(command = self.text.yview)
+
+
+root = tk.Tk()
+window = MainWindow(root)
+root.mainloop()
 
 
 # import docx
